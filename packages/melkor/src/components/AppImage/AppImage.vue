9c88738025ce48ba0a447c51caf085b361fcbfa2
Lucas Chapuis <chapuis.lucas74@gmail.com>
2025-03-11 16:23:30 +0100

    feat: add AppImage

---
+++ packages/melkor/src/components/AppImage/AppImage.vue
@@ -0,0 +1,93 @@
+<template>
+  <div
+    v-theme="theme"
+    class="mk-AppImage"
+    :data-ratio="ratio !== 'initial' || undefined"
+    :data-fit="fit ?? undefined"
+  >
+    <div class="mk-AppImage-shape">
+      <img
+        :src="props.src"
+        :alt="props.alt"
+        :title="props.title"
+      >
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { useTheme } from '../../composables';
+
+const props = defineProps<Props>();
+
+enum Fit {
+  cover = 'cover',
+  contain = 'contain',
+}
+
+interface Props {
+  src: string;
+  alt?: string;
+  title?: string;
+  ratio?: [number, number];
+  cover?: boolean;
+  contain?: boolean;
+}
+
+const theme = useTheme();
+
+const ratio = computed(() => {
+  if (props.ratio && props.ratio[0] && props.ratio[1]) {
+    return `${(props.ratio[1] / props.ratio[0]) * 100}%`;
+  }
+  return 'initial';
+});
+
+const fit = computed(() => {
+  if (props.cover) {
+    return Fit.cover;
+  }
+  if (props.contain) {
+    return Fit.contain;
+  }
+  return null;
+});
+</script>
+
+<style lang="scss">
+.mk-AppImage {
+  $this: &;
+
+  position: relative;
+
+  &[data-ratio='true'] {
+    #{$this} {
+      &-shape {
+        width: 100%;
+        padding-top: v-bind('ratio');
+      }
+    }
+
+    img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  &[data-fit='contain'] {
+    img {
+      object-fit: contain;
+    }
+  }
+
+  &[data-fit='cover'] {
+    img {
+      object-fit: cover;
+    }
+  }
+}
+</style>
