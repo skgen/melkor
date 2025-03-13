382e6b08f4fc6e50319a09e1964285447e629537
Lucas Chapuis <chapuis.lucas74@gmail.com>
2025-03-13 22:09:00 +0100

    feat: export Shape from nuxt

--- packages/melkor-nuxt/src/namespaces/load-features.ts
+++ packages/melkor-nuxt/src/namespaces/load-features.ts
@@ -3,10 +3,13 @@ import { addImportsSources } from '@nuxt/kit';
 export function loadFeatures(): void {
   type FeaturesExports = (keyof typeof import('@skgn/melkor/features'))[];
 
+  const features = [
+    'Theme',
+    'Shape',
+  ] as FeaturesExports;
+
   addImportsSources({
     from: '@skgn/melkor/features',
-    imports: [
-      'Theme',
-    ] as FeaturesExports,
+    imports: features,
   });
 }
